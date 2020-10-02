8f4e81156c20b9a02bb7cdad56b03bfb35d41363
임준혁 <imjunhyeok@imjunhyeog-ui-MacBookPro.local>
2020-10-03 00:38:21 +0900

    jpa 실습 강의 환경설정

---
+++ src/main/java/com/example/reporttroller/ReportTrollerApplication.java
@@ -0,0 +1,13 @@
+package com.example.reporttroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportTrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportTrollerApplication.class, args);
+	}
+
+}
