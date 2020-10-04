f493de607e698c746add838f997700f2c596a745
rottoy <wnsgur9609@naver.com>
2020-10-04 19:39:41 +0900

    테스트 검증 및 쿼리 파라미터 로그 기능 추가

---
+++ src/test/java/com/example/reporttroller/controller/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.example.reporttroller.controller;
+
+import com.example.reporttroller.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then : 검증
+        //AssertJ 라는 라이브러리를 스프링test가 dependency를 자동으로 가지고 있음.
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+        System.out.println("findmember == member : "+(findMember==member));
+        //findMember == member
+    }
+
+
+}
