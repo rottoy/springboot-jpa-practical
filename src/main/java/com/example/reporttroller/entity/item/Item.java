8a77500c446bf580aadbf521030dfab04a316742
임준혁 <imjunhyeok@imjunhyeog-ui-MacBookPro.local>
2020-10-06 18:22:15 +0900

    h2 인-메모리 환경 설정 및 Main , Test 모듈 환경설정 파일 분리

--- src/main/java/com/example/reporttroller/entity/item/Item.java
+++ src/main/java/com/example/reporttroller/entity/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.reporttroller.entity.item;
 
 import com.example.reporttroller.entity.Category;
+import com.example.reporttroller.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -33,4 +34,21 @@ public abstract class Item {
     private List<Category> categories = new ArrayList<>();
 
 
+    //==비즈니스 로직 ==//
+    /*
+    stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+    /*
+    * stock 감소
+    * */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 }
