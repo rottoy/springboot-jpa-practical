322f25fee8afa303ef05683b3123110621ef4774
rottoy <wnsgur9609@naver.com>
2020-10-06 18:22:15 +0900

    h2 인-메모리 환경 설정 및 Main , Test 모듈 환경설정 파일 분리

--- src/test/java/com/example/reporttroller/service/MemberServiceTest.java
+++ src/test/java/com/example/reporttroller/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,7 @@ public class MemberServiceTest {
     MemberRepository memberRepository;
 
     @Test
+    @Rollback(false)
     public void 회원가입() throws Exception{
 
         //given - 얘가 주어질 꺼야
@@ -37,13 +39,22 @@ public class MemberServiceTest {
 
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
+    //@Rollback(false)
     public void 중복_회원_예외() throws Exception{
-       //given
+        //given
+        Member member1=new Member();
+        member1.setName("kim11");
 
-       //when
+        Member member2=new Member();
+        member2.setName("kim11");
 
-       //then
+        //when
+        memberService.join(member1);
+        memberService.join(member2);
 
+
+        //then
+        fail("예외가 발생해야 한다.");
     }
 }
