4c4f13e262f1e0b423caf8998068a0b30b99b646
rottoy <wnsgur9609@naver.com>
2020-10-09 16:51:51 +0900

    OrderService 테스트 추가

---
+++ src/test/java/com/example/reporttroller/service/OrderServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.reporttroller.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    
+    @Test
+    public void 상품주문() throws Exception{
+       //given
+       
+       //when
+       
+       //then
+    
+    }
+    
+    @Test
+    public void 재고수량초과() throws Exception{
+       //given
+       
+       //when
+       
+       //then
+    
+    }
+}
